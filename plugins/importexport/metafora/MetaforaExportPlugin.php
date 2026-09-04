e6edb27dad5432c34e50ee7e28bc45c9dbd4bb2b
damatveev <34768012+damatveev@users.noreply.github.com>
2026-09-04 15:51:29 +0300

    Add configurable Metafora API settings
    * Add per-journal Metafora plugin settings handler
    
    * Add Metafora settings form
    
    * Add Metafora settings template
    
    * Complete Metafora settings form options
    
    * Add Russian settings translations
    
    * Add English settings translations

--- plugins/importexport/metafora/MetaforaExportPlugin.php
+++ plugins/importexport/metafora/MetaforaExportPlugin.php
@@ -2,10 +2,21 @@
 
 namespace APP\plugins\importexport\metafora;
 
+use APP\notification\NotificationManager;
+use PKP\core\JSONMessage;
 use PKP\plugins\ImportExportPlugin;
 
 class MetaforaExportPlugin extends ImportExportPlugin
 {
+    public function register($category, $path, $mainContextId = null): bool
+    {
+        $success = parent::register($category, $path, $mainContextId);
+        if ($success) {
+            $this->addLocaleData();
+        }
+        return $success;
+    }
+
     public function getName(): string
     {
         return 'MetaforaExportPlugin';
@@ -19,5 +30,41 @@ class MetaforaExportPlugin extends ImportExportPlugin
     public function getDescription(): string
     {
         return __('plugins.importexport.metafora.description');
+    }
+
+    public function getPluginSettingsPrefix(): string
+    {
+        return 'metafora';
+    }
+
+    public function manage($args, $request): JSONMessage
+    {
+        $context = $request->getContext();
+        if (!$context) {
+            return parent::manage($args, $request);
+        }
+
+        $this->addLocaleData();
+        $form = new MetaforaSettingsForm($this, $context->getId());
+
+        switch ($request->getUserVar('verb')) {
+            case 'index':
+                $form->initData();
+                return new JSONMessage(true, $form->fetch($request));
+
+            case 'save':
+                $form->readInputData();
+                if ($form->validate()) {
+                    $form->execute();
+                    $user = $request->getUser();
+                    if ($user) {
+                        (new NotificationManager())->createTrivialNotification($user->getId());
+                    }
+                    return new JSONMessage(true);
+                }
+                return new JSONMessage(true, $form->fetch($request));
+        }
+
+        return parent::manage($args, $request);
     }
 }
---
+++ plugins/importexport/metafora/MetaforaSettingsForm.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace APP\plugins\importexport\metafora;
+
+use APP\template\TemplateManager;
+use PKP\form\Form;
+use PKP\form\validation\FormValidatorCSRF;
+use PKP\form\validation\FormValidatorPost;
+use PKP\plugins\Plugin;
+
+class MetaforaSettingsForm extends Form
+{
+    public int $contextId;
+    public Plugin $plugin;
+
+    public function __construct(Plugin $plugin, int $contextId)
+    {
+        $this->contextId = $contextId;
+        $this->plugin = $plugin;
+        parent::__construct($plugin->getTemplateResource('settingsForm.tpl'));
+        $this->addCheck(new FormValidatorPost($this));
+        $this->addCheck(new FormValidatorCSRF($this));
+    }
+
+    public function initData(): void
+    {
+        foreach ($this->getFormFields() as $fieldName => $fieldType) {
+            $this->setData($fieldName, $this->plugin->getSetting($this->contextId, $fieldName));
+        }
+        if (!$this->getData('exportFormat')) {
+            $this->setData('exportFormat', 'api');
+        }
+    }
+
+    public function readInputData(): void
+    {
+        $this->readUserVars(array_keys($this->getFormFields()));
+    }
+
+    public function execute(...$functionArgs)
+    {
+        parent::execute(...$functionArgs);
+        foreach ($this->getFormFields() as $fieldName => $fieldType) {
+            $this->plugin->updateSetting($this->contextId, $fieldName, $this->getData($fieldName), $fieldType);
+        }
+    }
+
+    public function fetch($request, $template = null, $display = false)
+    {
+        TemplateManager::getManager($request)->assign('exportFormats', [
+            'api' => __('plugins.importexport.metafora.settings.format.api'),
+            'journal' => __('plugins.importexport.metafora.settings.format.journal'),
+            'jats' => __('plugins.importexport.metafora.settings.format.jats'),
+            'scienceSpace' => __('plugins.importexport.metafora.settings.format.scienceSpace'),
+        ]);
+        return parent::fetch($request, $template, $display);
+    }
+
+    public function getFormFields(): array
+    {
+        return [
+            'apiUrl' => 'string',
+            'apiToken' => 'string',
+            'exportFormat' => 'string',
+            'validateXml' => 'bool',
+            'includePdf' => 'bool',
+            'includeReferences' => 'bool',
+            'autoExport' => 'bool',
+        ];
+    }
+}
